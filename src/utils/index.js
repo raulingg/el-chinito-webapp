00716393cc73d38944056dc661739a247a7bfb6a
Raul Quispe (raulingg) <relaxedchild@gmail.com>
2020-07-25 22:01:26 -0500

    refactor(utils): Add makeFormatter and subtractDate functions.

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,9 @@ export const currencyFormatter = new Intl.NumberFormat('es-PE', {
   style: 'currency',
   currency: 'PEN',
 })
+
+export const makeFormatter = ({ locale = 'default', options = {} } = {}) =>
+  new Intl.DateTimeFormat(locale, { timeZone: 'America/Lima', ...options })
+
+export const subtractDate = ({ date = new Date(), days = 1 }) =>
+  new Date(new Date().setTime(date.getTime() - 3600000 * 24 * days))
