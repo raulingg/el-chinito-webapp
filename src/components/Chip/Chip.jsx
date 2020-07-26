235ee6406daac8e6db45a2a5833a67eb1ffc2b93
Raul Quispe (raulingg) <relaxedchild@gmail.com>
2020-07-25 21:57:22 -0500

    feat(components): Add Chip

---
+++ src/components/Chip/Chip.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './chip.css'
+import PropTypes from 'prop-types'
+
+const Chip = ({ label = 'Chip', color = 'default' }) => (
+  <div className={`chip chip--color-${color}`}>
+    <span>{label}</span>
+  </div>
+)
+
+Chip.propTypes = {
+  label: PropTypes.string,
+  color: PropTypes.string,
+}
+
+export default Chip
