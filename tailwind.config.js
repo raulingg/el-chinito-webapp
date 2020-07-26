8ec210e4decb79465ed4e4bd0945b19c9773187a
Raul Quispe (raulingg) <relaxedchild@gmail.com>
2020-07-25 21:56:53 -0500

    refactor(styles): Override Tailwind's default fontsize.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   theme: {
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      tiny: '.875rem',
+      base: '1.25rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
     extend: {
       colors: {
         brand: {
@@ -13,6 +27,7 @@ module.exports = {
     },
   },
   variants: {
+    tableLayout: ['responsive', 'hover', 'focus'],
     flexDirection: ['responsive', 'hover', 'focus', 'active'],
     backgroundColor: ['responsive', 'hover', 'focus', 'active', 'disabled'],
   },
