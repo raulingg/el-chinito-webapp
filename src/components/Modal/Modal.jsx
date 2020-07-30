785161ccba9fb8fb11b29db8d6656f377f800e55
Raul Quispe (raulingg) <relaxedchild@gmail.com>
2020-07-29 22:27:33 -0500

    refactor(modal-component): Add backdrop click and escape keydown feats to close

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 import './modal.css'
 
-const Modal = ({ open, maxWidth, children }) => {
+const Modal = ({
+  open,
+  maxWidth,
+  disableBackdropClick,
+  disableEscapeKeyDown,
+  onClose,
+  children,
+}) => {
+  useEffect(() => {
+    if (disableEscapeKeyDown || !open) {
+      return
+    }
+
+    const handleKeydown = (e) => {
+      e.stopPropagation()
+
+      return e.code === 'Escape' && onClose()
+    }
+
+    document.addEventListener('keydown', handleKeydown)
+
+    return () => document.removeEventListener('keydown', handleKeydown)
+  }, [open])
+
+  const handleClick = (e) => {
+    e.stopPropagation()
+    if (disableBackdropClick) return
+
+    const [modal] = document.getElementsByClassName('modal')
+
+    if (modal != e.target) return
+
+    onClose()
+  }
+
   return (
-    <div className="modal" hidden={!open}>
+    <div className="modal" hidden={!open} onClick={handleClick}>
       <div className={`modal__container max-w-${maxWidth}`}>{children}</div>
     </div>
   )
@@ -14,11 +48,16 @@ const Modal = ({ open, maxWidth, children }) => {
 Modal.defaultProps = {
   open: false,
   maxWidth: 'md',
+  disableBackdropClick: false,
+  disableEscapeKeyDown: false,
 }
 
 Modal.propTypes = {
   open: PropTypes.bool.isRequired,
-  maxWidth: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  disableBackdropClick: PropTypes.bool,
+  disableEscapeKeyDown: PropTypes.bool,
+  onClose: PropTypes.func,
+  maxWidth: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl', '2xl']),
 }
 
 export default Modal
